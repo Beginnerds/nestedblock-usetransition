821c949f05ca79d99ed9a3ac8b43fc162a6c7228
Shubham <shubhamkalra619@gmail.com>
2025-03-03 10:48:40 +0530

    with useTransition

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useTransition } from 'react'
 import './App.css'
 import { Button } from './components/ui/button'
 import About from './components/tabs/about'
@@ -9,17 +9,21 @@ type Tab = 'about' | 'messages' | 'friends'
 
 function App() {
 
+  const [isPending,startTransition] = useTransition()
+
   const [currentTab,setCurrentTab] = useState<Tab>('about')
 
   const updateTab = (tab:Tab) => {
-    setCurrentTab(tab)
+    startTransition(()=>{
+      setCurrentTab(tab)
+    })
   }
 
   return (
    <div className='min-w-screen min-h-screen bg-slate-500 py-20 px-10'>
       <div className='w-full lg:w-3/4 mx-auto flex justify-center items-center gap-4'>
           <Button className='cursor-pointer' onClick={()=>updateTab('about')} size={'lg'}>About</Button>
-          <Button className='cursor-pointer' onClick={()=>updateTab('messages')} size={'lg'}>Messages</Button>
+          <Button className='cursor-pointer' onClick={()=>updateTab('messages')} size={'lg'}>Messages (Slow)</Button>
           <Button className='cursor-pointer' onClick={()=>updateTab('friends')} size={'lg'}>Friends</Button>
       </div>
       <div className='w-full max-w-lg mx-auto mt-10 bg-slate-400 min-h-[400px] p-4 rounded-lg'>
